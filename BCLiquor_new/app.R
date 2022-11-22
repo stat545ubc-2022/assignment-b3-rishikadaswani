4bd3a0609099fac47552b67e600d7de881e07aa4
rishikadaswani <rishikad@student.ub.ca>
2022-11-22 12:20:18 -0800

    assignment 3 commit

---
+++ BCLiquor_new/app.R
@@ -0,0 +1,69 @@
+
+library(shiny)
+library(tidyverse)
+library(dplyr)
+
+bcl <- read.csv("bcl-data.csv")
+
+ui <- fluidPage(
+  titlePanel("BC Liquor Store Data"),
+  h5("Exploring the BC Liquor Store Dataset - choose the right liquor for you!"),
+  #Feature 1: Add image to the UI
+  img(src = "bcliquor_logo.png", align = "center"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("PriceInput", "Price", 0, 100,
+                  value = c(25,40), pre = "$"),
+      radioButtons("typeInput", "Type", 
+                   choices = c("BEER", "REFRESHMENTS", "SPIRITS", "WINE")),
+      uiOutput("countryOutput"),
+   
+    ),
+    mainPanel(
+      #Feature 2: Add text to show how many results have been found 
+      textOutput("text"),
+      #Feature 3: Creating individual tabs for the plot and the table 
+      tabsetPanel(
+        tabPanel("Plot", plotOutput("alcohol_hist")), 
+      #Feature 4: Create an Interactive Table 
+        tabPanel("Table", DT::dataTableOutput("data_table"))
+      )
+    )
+  ), 
+  a(href = "https://github.com/daattali/shiny-server/blob/master/bcl/data/bcl-data.csv", "Link to the original data set")
+)
+
+server <- function(input, output) {
+  
+  output$countryOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
+  }) 
+  
+  filtered_data <- 
+    reactive({
+      bcl %>% filter(Price >= input$PriceInput[1] &
+                       Price <= input$PriceInput[2] & 
+                       Type == input$typeInput)
+      
+    })
+  
+  output$alcohol_hist <- 
+    renderPlot({
+      filtered_data() %>%
+        ggplot(aes(Alcohol_Content)) + geom_histogram() 
+    })
+  
+
+  #Feature 4: Interactive table 
+  output$data_table <- 
+    DT::renderDataTable({
+      filtered_data()
+    })
+  
+  #Feature 2: Adding text 
+  output$text <- renderText(paste("We found", nrow(filtered_data()), "options for you!"))
+}
+
+shinyApp(ui = ui, server = server)
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+# STAT545B: Assignment B3 - BC Liquor Store Shiny App 
+This is the repository for Assignment B3 - **Option A**: Adding Features to the BC Liquor App 
+Link to the app - https://rishikadaswani.shinyapps.io/bcliquor_new/  
 
+## New Features 
+**Feature 1:** Added image (BC Liquor Logo) to the UI. 
+This feature improves the overall aesthetic of the app and helps make the app more visually interesting 
+
+**Feature 2:** Added Text to show how many results have been found 
+This feature shows the number of results found whenever the filter changes. It makes it easier for the user to understand how many options are available. 
+
+**Feature 3:** Made individual tabs for the plot and the table 
+Individual tabs for plot and table improve the overall aesthetic keeping the app neat and tidy 
+
+**Feature 4:** Made an interactive table 
+The interactive table allows for users to search and sort the data in order to allow for better exploration 
+
+## Acknowledgements 
+This app is based on the BC Liquor App by Dean Attali. The link to the full dataset can be found in the app itself and https://github.com/daattali/shiny-server/blob/master/bcl/data/bcl-data.csv here. It was retrieved from OpenDataBC. 
